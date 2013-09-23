c5cb40f56dbcb4bd34947f8b23d0a872d162a05f
sandeep2400 <gopal.sandeep@gmail.com>
2013-09-23 14:29:53 -0400

    Add new emailconf.php
    emailconf,php is a template to send an email (text or html) confirmation
    to an email address passed as parameter. The data to be entered is the
    From address, the smtp server, smpt username and password, port. You can
    get this information from your hosting provider.
    You can choose to send an html or text email, and attach a file . If you
    attach a file make sure to look up the mime type of the file.

---
+++ emailconf.php
@@ -0,0 +1,54 @@
+<?php 
+function sendmail($fax_num) {
+define("EMAIL_FROM", "from email address");
+define("EMAIL_TO", "to email address from ioemail");
+define("SMTP_SERVER", "smtp.office365.com");
+define("SMTP_USER", "Full username");
+define("SMTP_PASSWORD", "Password");
+define("SMTP_PORT", "587");
+
+require_once ('/Mail/Mail.php');
+require_once ('/Mail/mime.php');
+require_once ('/Net_SMTP/SMTP.php');
+$from = EMAIL_FROM;
+$to = EMAIL_TO;
+$port = SMTP_PORT;
+$subject = "RMA Document for Dreamline: ".$fax_num;
+
+$headers = array ('From' => $from,'To' => $to, 'Subject' => $subject);
+$text = 'Please look at the document for RMA instructions.'; //write the confirmation email here
+
+/*		$html = '<html>';
+$html .= '<head>';
+$html .= '</head><body><h4>Please look at the document for RMA instructions.</h4></body></html>';
+*/		
+//		$html = "<html></html>";
+//$file = 'files/Lowes_RMA.docx'; // attachment
+$crlf = "\n";
+
+$mime = new Mail_mime($crlf);
+//		$mime->setHTMLBody($html);
+$mime->setTXTBody($text);
+//		$mime->addAttachment($file, 'text/plain');
+$mime->addAttachment($file, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+
+$body = $mime->get();
+$headers = $mime->headers($headers);
+
+$host = SMTP_SERVER;
+$username = SMTP_USER;
+$password = SMTP_PASSWORD;
+
+$smtp = Mail::factory('smtp', 
+		array ('host' => $host,
+			   'port' => $port, 
+			   'auth' => true,
+			   'username' => $username,
+			   'password' => $password));
+
+$mail = $smtp->send($to, $headers, $body);
+if (PEAR::isError($mail)) {
+  echo($mail->getMessage());
+}	
+}
+?>
